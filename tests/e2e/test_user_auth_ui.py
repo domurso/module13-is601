6abbd4bd9360041bc1d070a2ff9f029e06de7b97
Dom Urso <duom22@gmail.com>
2025-07-29 00:21:21 -0400

    aiming at wrong url

--- tests/e2e/test_user_auth_ui.py
+++ tests/e2e/test_user_auth_ui.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright, expect
 import time
 
 # Base URL of the application
-BASE_URL = "http://127.0.0.1:8001"
+BASE_URL = "http://127.0.0.1:8000"
 
 # Helper function to generate unique username
 def generate_unique_username():
